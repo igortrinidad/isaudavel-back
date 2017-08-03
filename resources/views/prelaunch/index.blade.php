db8332770a362cb4763144c4dc8522adefe97ef8
Igor Trindade <igorlucast@hotmail.com>
2017-08-03 00:24:55 -0300

    Added analytics tracker

--- resources/views/prelaunch/index.blade.php
+++ resources/views/prelaunch/index.blade.php
@@ -216,8 +216,9 @@
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-70761422-5', 'auto');
+          ga('create', 'UA-70761422-7', 'auto');
           ga('send', 'pageview');
+
         </script>
         
     </body>
