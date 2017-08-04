a883ec924da84adfb6bbfbb9e8eb9b7cff31d269
Igor Trindade <igorlucast@hotmail.com>
2017-08-04 09:25:56 -0300

    Added hotjar tracking code --update

--- resources/views/prelaunch/index.blade.php
+++ resources/views/prelaunch/index.blade.php
@@ -30,11 +30,11 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ elixir('build/prelaunch/css/build_vendors_custom.css') }}">
 
-        <!-- Hotjar Tracking Code for https://weskd.com -->
+        <!-- Hotjar Tracking Code for https://isaudavel.com -->
         <script>
             (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:578282,hjsv:5};
+                h._hjSettings={hjid:583813,hjsv:5};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
