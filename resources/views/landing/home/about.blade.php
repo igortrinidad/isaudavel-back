a926775e985b22e3f637409b874686d2ec0e1f1a
Andre Brandao <andrebf4@gmail.com>
2017-08-25 12:04:46 -0300

    small changes in home for new landing

--- resources/views/landing/home/about.blade.php
+++ resources/views/landing/home/about.blade.php
@@ -1,11 +1,17 @@
- <section id="contact" class="section">
+<style media="screen">
+    .section{ background: #F4F5F5; }
+</style>
+
+<section id="contact" class="section">
     <div class="container">
         <div class="row">
 
             <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="block">
-                    <div class="text-center wow fadeInUp">
-                        <h2>Para você</h2>
+                <div class="card wow fadeInUp">
+                    <div class="card-header ch-alt text-center">
+                        <h2 class="f-300">Para você</h2>
+                    </div>
+                    <div class="card-body card-padding">
                         <p class="f-14 m-t-10">No <b>iSaudavel</b> você encontrará profissionais especializados em sua saúde como personal trainer, nutricionista, estúdios de pilates, academia, fisioterapia, crossfit e diversas clínicas de saúde e bem estar - todos unidos em só lugar e você poderá compartilhar as principais informações sobre sua saúde e objetivos com esses profissionais, que juntos irão te ajudar a atingir seus objetivos de saúde, estética e bem estar.</p>
                     </div>
                 </div>
@@ -14,13 +20,15 @@
 
 
         <div class="row m-t-30">
-            <div class="col-xs-12 col-sm-12 col-md-12 wow fadeInUp">
-				<div class="block text-left">
-					<div class="text-center">
-						<h2>Para profissinoais</h2>
-						<p class="f-14 m-t-10">O <b>iSaudavel</b> foi criado para você profissional da área da saúde e seu cliente economizarem o mais importante da vida: <b>TEMPO</b>. Uma rede social dedicada para você divulgar seus serviços e organizar o atendimento a seu cliente de forma simplificada e objetiva, integrando outros profissionais que assim como você estão comprometidos à promover a saúde e bem estar de seus clientes.</p>
-					</div>
-				</div>
+            <div class="col-xs-12 col-sm-12 col-md-12 ">
+    			<div class="card text-left wow fadeInUp">
+    				<div class="card-header ch-alt text-center">
+    					<h2 class="f-300">Para profissinoais</h2>
+    				</div>
+                    <div class="card-body card-padding">
+                        <p class="f-14 m-t-10">O <b>iSaudavel</b> foi criado para você profissional da área da saúde e seu cliente economizarem o mais importante da vida: <b>TEMPO</b>. Uma rede social dedicada para você divulgar seus serviços e organizar o atendimento a seu cliente de forma simplificada e objetiva, integrando outros profissionais que assim como você estão comprometidos à promover a saúde e bem estar de seus clientes.</p>
+                    </div>
+    			</div>
             </div>
 
         </div>
