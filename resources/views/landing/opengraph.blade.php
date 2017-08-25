672f65cd073a109c75eff9d55dacf3e1ce328549
Igor Trindade <igorlucast@hotmail.com>
2017-08-25 11:20:49 -0300

    Minor changes on opengraph --merge-and-push

--- resources/views/landing/opengraph.blade.php
+++ resources/views/landing/opengraph.blade.php
@@ -4,6 +4,8 @@
 	$routeName = \Request::route()->getName();
 	$category_query = \Request::query('category');
 
+	$current_url = \Request::fullUrl();
+
 	if($category_query){
 		$category_query = \App\Models\Category::where('slug', ($category_query))->first(); 
 	}
@@ -36,7 +38,7 @@
 	@else
 		<meta property="og:image" content="{{$category_query->avatar}}">
 		<meta property="og:image:type" content="image/png">
-		<meta property="og:url" content="{{url()->current()}}">
+		<meta property="og:url" content="{{ $current_url }}">
 		<meta property="og:title" content="{{$category_query->name}}: encontre os melhores profissionais no iSaudavel.">
 		<meta property="og:description" content="Profissionais para {{$category_query->name}} e outras especialidades para ajudar você a cuidar de sua saúde, bem estar e estética.">
 	@endif
@@ -44,7 +46,7 @@
 
 <!-- LANDING COMPANIES SHOW -->
 @if($routeName == 'landing.companies.show')
-	<meta property="og:url" content="{{url()->current()}}">
+	<meta property="og:url" content="{{ $current_url }}">
 	<meta property="og:site_name" content="iSaudavel">
 	<meta property="og:description" content="iSaudavel é uma ferramenta para conectar você e os melhores profissionais para cuidar da sua saúde.">
 	<meta property="og:image" content="{{$company_fetched->avatar}}">
@@ -54,7 +56,7 @@
 
 <!-- LANDING INDEX -->
 @if($routeName == 'landing.professionals.show')
-	<meta property="og:url" content="{{url()->current()}}">
+	<meta property="og:url" content="{{ $current_url }}">
 	<meta property="og:title" content="iSaudavel: {{$professional_fetched->full_name}}">
 	<meta property="og:site_name" content="iSaudavel">
 	<meta property="og:description" content="iSaudavel é uma ferramenta para conectar você e os melhores profissionais para cuidar da sua saúde.">
