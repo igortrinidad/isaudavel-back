ff8459e2b88a9f38279697a0f213a698772c932d
Andre Brandao <andrebf4@gmail.com>
2017-08-25 15:34:18 -0300

    some fixes in header when has video and when has no video

--- resources/views/landing/home/video.blade.php
+++ resources/views/landing/home/video.blade.php
@@ -2,34 +2,50 @@
     .section{ background: #F4F5F5; }
 
     .video-container {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        height: 600px;
+        overflow: hidden;
     }
     .video-container video {
-    /* Make video to at least 100% wide and tall */
-    min-width: 100%;
-    min-height: 100%;
-    /* Setting width & height to auto prevents the browser from stretching or squishing the video */
-    width: auto;
-    height: auto;
-    /* Center the video */
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
+        min-width: 100%;
+        min-height: 100%;
+        width: auto;
+        height: auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .slogan {
+        position: relative;
+        font-size: 80px;
+        line-height: 80px;
+        margin-top: 100px;
+        max-width: 768px;
+        color: #f4f5f5;
+        font-weight: 400;
+    }
+
+    .screenshot {
+        position: relative;
+        max-width: 230px;
+        float: right;
     }
 
 </style>
+<div>
+    <div class="video-container">
+      <video autoplay loop="true" width="1280" height="720">
+        <source type="video/mp4" src="https://coverr.co/s3/mp4/Strole_in_the_Park.mp4">
+      </video>
+    </div>
 
-<div class="video-container">
-
-  <video autoplay loop="true" width="1280" height="720">
-    <source type="video/mp4" src="https://coverr.co/s3/mp4/Strole_in_the_Park.mp4">
-  </video>
-
+    <div class="container">
+        <h2 class="slogan">A sua saúde em boas <strong class="f-700">mãos</strong>.</h2>
+        <img class="screenshot" src="https://weplaces.com.br/assets/weplaces/iphone_black/screen1.png" alt="Screen 1">
+    </div>
 </div>
