81126f29d647bc90d4b040733a25458973521153
Igor Trindade <igorlucast@hotmail.com>
2017-08-25 11:30:58 -0300

    More changes on opengraph --merge-and-push

--- resources/views/landing/opengraph.blade.php
+++ resources/views/landing/opengraph.blade.php
@@ -10,24 +10,23 @@
 		$category_query = \App\Models\Category::where('slug', ($category_query))->first(); 
 	}
 ?>
+	
+	<!-- GLOBAL -->
+	<meta property="fb:app_id" content="1854829291449231" />
+	<meta property="og:locale" content="pt_BR">
+	<meta property="og:site_name" content="iSaudavel">
 
 <!-- LANDING INDEX -->
 @if($routeName == 'landing.index')
-	<meta property="fb:app_id" content="1854829291449231" />
-	<meta property="og:locale" content="pt_BR">
+	
 	<meta property="og:url" content="https://isaudavel.com">
 	<meta property="og:title" content="iSaudavel">
-	<meta property="og:site_name" content="iSaudavel">
 	<meta property="og:description" content="iSaudavel é uma ferramenta para conectar você e os melhores profissionais para cuidar da sua saúde.">
 	<meta property="og:image" content="https://isaudavel.com/logos/LOGO-1-02.png">
 	<meta property="og:image:type" content="image/png">
 @endif
 
 @if($routeName == 'landing.search.index')
-	<meta property="fb:app_id" content="1854829291449231" />
-	<meta property="og:locale" content="pt_BR">
-	<meta property="og:site_name" content="iSaudavel">
-	
 
 	@if( !$category_query )
 		<meta property="og:image" content="https://isaudavel.com/logos/LOGO-1-02.png">
@@ -47,7 +46,6 @@
 <!-- LANDING COMPANIES SHOW -->
 @if($routeName == 'landing.companies.show')
 	<meta property="og:url" content="{{ $current_url }}">
-	<meta property="og:site_name" content="iSaudavel">
 	<meta property="og:description" content="iSaudavel é uma ferramenta para conectar você e os melhores profissionais para cuidar da sua saúde.">
 	<meta property="og:image" content="{{$company_fetched->avatar}}">
 	<meta property="og:title" content="iSaudavel: {{$company_fetched->name}}">
@@ -58,7 +56,6 @@
 @if($routeName == 'landing.professionals.show')
 	<meta property="og:url" content="{{ $current_url }}">
 	<meta property="og:title" content="iSaudavel: {{$professional_fetched->full_name}}">
-	<meta property="og:site_name" content="iSaudavel">
 	<meta property="og:description" content="iSaudavel é uma ferramenta para conectar você e os melhores profissionais para cuidar da sua saúde.">
 	<meta property="og:image" content="{{$professional_fetched->avatar}}">
 	<meta property="og:image:type" content="image/png">
