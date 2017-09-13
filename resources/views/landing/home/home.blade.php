c9364bb6ff90eb89645ca3fa62346bf9d806ef81
Andre Brandao <andrebf4@gmail.com>
2017-09-13 10:36:49 -0300

    some changes in landing home

--- resources/views/landing/home/home.blade.php
+++ resources/views/landing/home/home.blade.php
@@ -6,19 +6,41 @@
         a, a:hover{ color: #383939; text-decoration: none; }
         .min-height-card { min-height: 370px; }
         .section.divider{
-            background-color: #fff;
-            background-image: url('/images/bg_test.png') !important;
+            background-color: transparent;
+            background-image: url('https://image.ibb.co/ncHZ1v/home.jpg') !important;
             background-size: cover;
             background-position: center center;
             background-repeat: no-repeat;
             background-attachment: fixed
         }
+        /*.section.divider {
+            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVQoU2NkIAIYWhtKMRJSB1J0/uj5Z3gVwhSBDMOpEFkRToXoirAqxKYIQyEuRSgK8SmCKySkCKyQGEUghQDZYyTCcREOzgAAAABJRU5ErkJggg==);
+            background-repeat: repeat;
+            background-attachment: fixed;
+
+        }*/
+
+        /*.section.divider {
+            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQ0lEQVQoU2NkIBIwIqsztDaUgvHPHz3/DFkOrhBZETbFYIXoJmEzGUUhsnUwxTAx0kxEtx6nG2ESRPmaUHCihCM+xQCj5ygLviR2SQAAAABJRU5ErkJggg==);
+            background-repeat: repeat;
+            background-color: #fff;
+        }*/
+
+        /*.section.divider {
+            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPElEQVQoU2M0tDaUOn/0/DMGAoARJE+MYrBCYhTDFRJSjKIQn2IMhbgUY1WITTFOheiK8SpEVkxQIUwxAJXnJMK1cQiCAAAAAElFTkSuQmCC);
+            background-repeat: repeat;
+        }*/
     </style>
 
     <section id="contact" class="section divider">
         <div class="container">
 
-            <h2 class="text-center">Para você e profissionais de saúde</h2>
+            <div class="text-center">
+                <h2 class="f-700 text-center m-b-10" style="color: #fff;">Para você e profissionais de saúde</h2>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60px" height="20px" viewBox="0 0 60 20" enable-background="new 0 0 60 20" xml:space="preserve">
+                    <path fill="none" stroke="#FFFFFF" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" d="M3.188,8.848 C20.521,3.89,38.313,16.66,56.813,9.473" style="stroke-dasharray: 54.3116, 54.3116; stroke-dashoffset: 0;"></path>
+                </svg>
+            </div>
 
             <div class="row m-t-30">
                 <div class="col-xs-12 col-sm-6 col-md-6">
