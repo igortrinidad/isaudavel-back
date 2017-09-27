553971b073565f437c3743a45f0a1f9206afe9aa
Andre Brandao <andrebf4@gmail.com>
2017-09-27 18:24:45 -0300

    added call to download in others landing section

--- resources/views/landing/companies/show.blade.php
+++ resources/views/landing/companies/show.blade.php
@@ -12,6 +12,7 @@
         color: #383938;
         text-decoration: none;
     }
+    .store-badge { margin: 0 auto; }
 
     .btn.btn-facebook{ background-color: #3b5998; color: #F4F5F5; }
     .btn.btn-whatsapp{ background-color: #1ebea5; color: #F4F5F5; }
@@ -419,6 +420,32 @@
             @endif
         </div>
         <!-- / MAP -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body card-padding">
+                            <!-- Call To Download -->
+                            <div class="text-center">
+                                <h3 class="f-300">Baixe o <strong style="color: #72c157">iSaudavel</strong> e interaja com empresas e outros usuários</h3>
+                                <div class="row">
+                                    <div class="col-sm-12 m-t-20">
+                                        <a style="display: inline-block;" href="https://play.google.com/store/apps/details?id=com.isaudavel" target="_blank" title="Faça o download na PlayStore para Android">
+                                            <img class="store-badge img-responsive" src="/images/play_store_btn.png" alt="Faça o download na PlayStore para Android">
+                                        </a>
+                                        <a style="display: inline-block;" href="https://itunes.apple.com/us/app/isaudavel/id1277115133?mt=8" target="_blank" title="Faça o download na APP Store para IOS">
+                                            <img class="store-badge img-responsive" src="/images/app_store_btn.png" alt="Faça o download na APP Store para IOS">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- / Call To Download -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- PHOTOS COL -->
         <div class="container">
