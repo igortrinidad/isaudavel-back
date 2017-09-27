edc35eeee3ea4784ba6636f316aeab9c75ed5ac9
Andre Brandao <andrebf4@gmail.com>
2017-09-27 10:57:27 -0300

    styles for recipe show

--- resources/views/landing/recipes/show.blade.php
+++ resources/views/landing/recipes/show.blade.php
@@ -3,7 +3,7 @@
 @section('landing-content')
 
     <style media="screen">
-
+    .store-badge { margin: 0 auto; }
     .event-name {
         height: 78px; width: 100%;
         position: relative;
@@ -54,7 +54,39 @@
     .fix-event {
         width: 100px;
     }
+    .img-recipe { border-radius: 4px; }
+
+    .ingredients { list-style: none; padding-left: 10px; }
+    .ingredients .item {
+        margin: 0;
+        margin-bottom: 1em;
+        padding-left: 1.5em;
+        position: relative;
+    }
+    .ingredients .item:after {
+        content: '';
+        height: 5px;
+        width: 5px;
+        background: #7ac358;
+        display: block;
+        position: absolute;
+        transform: rotate(45deg);
+        top: 50%;
+        left: 0;
+        margin-top: -2px;
+    }
+    .recipe-icon {
+        color: #7ac358;
+        font-size: 28px;
+    }
+    .recipe-box {
+        background: #fff;
+        border-radius: 4px;
+        padding: 10px;
+        margin-top: 30px;
+    }
     </style>
+
     <section id="events-show" class="section divider">
         <div class="event-avatar" style="background-image: url('{{ $recipe_fetched->avatar }}')">
         </div>
@@ -68,21 +100,121 @@
                 </div>
             </div>
 
-
-
             <div class="row m-t-30">
                 <!-- CENTER COL "ABOUT" -->
                 <div class="col-sm-9">
-                    <div class="card">
-                        <div class="card-header ch-alt text-center">
-                            <h2 class="f-400">Sobre o receita</h2>
+
+                    <!-- Recipe Informations -->
+                    <div class="row">
+                        <!-- Recipe Prep Time -->
+                        <div class="col-xs-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="recipe-box">
+                                        <i class="ion-android-alarm-clock recipe-icon"></i>
+
+                                        <h5 class="f-300 m-0 m-b-10">Preparo</h5>
+                                        <small class="f-300">{{ $recipe_fetched->prep_time }} min</small>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <!-- Recipe Prep Time -->
+
+                        <!-- Recipe Portions -->
+                        <div class="col-xs-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="recipe-box">
+                                        <i class="ion-android-restaurant recipe-icon"></i>
+
+                                        <h5 class="f-300 m-0 m-b-10">Rendimento</h5>
+                                        <small class="f-300">{{ $recipe_fetched->portions }} porções</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Recipe Portions -->
+
+                        <!-- Recipe Difficulty -->
+                        <div class="col-xs-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="recipe-box">
+                                         @for($i = 0; $i < $recipe_fetched->difficulty; $i++)
+                                             <i class="ion-fork recipe-icon m-r-5" v-for="n in $recipe_fetched->difficulty"></i>
+                                         @endfor
+                                         <h5 class="f-300 m-0 m-b-10">Dificuldade</h5>
+                                         @if($recipe_fetched->difficulty == 1)
+                                             <small class="f-300">Fácil</small>
+                                         @endif
+                                         @if($recipe_fetched->difficulty == 2)
+                                             <small class="f-300">Normal</small>
+                                         @endif
+                                         @if($recipe_fetched->difficulty == 3)
+                                             <small class="f-300">Difícil</small>
+                                         @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- / Recipe Difficulty -->
+
+                        <!-- Recipe Rating -->
+                        <div class="col-xs-6 text-center">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="recipe-box">
+                                        <i class="ion-ios-star recipe-icon"></i>
+
+                                        <h5 class="f-300 m-0 m-b-10">Avaliação</h5>
+                                        <small class="f-300">{{ $recipe_fetched->current_rating }}</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Recipe Rating -->
+
+                    </div>
+                    <!-- / Recipe Informations -->
+
+                    <div class="card">
                         <div class="card-body" style="padding: 6px;">
+                            <!-- Recipe Photos -->
+                            @if(count($recipe_fetched->photos) > 0)
+                                <div class="row row-event m-t-0 m-b-30">
+                                    @foreach($recipe_fetched->photos as $photo)
+                                    <div class="col-sm-3 event-col">
+                                        <img class="img-responsive img-recipe" src="{{ $photo->photo_url }}" alt="">
+                                    </div>
+                                    @endforeach
+                                </div>
+                            @else
+                                <h4 class="f-300">Este receita ainda não possui imagens</h4>
+                            @endif
+                            <!-- Recipe Photos -->
 
-                            <h4 class="f-300 m-t-15 m-b-10">Endereço:</h4>
-                            <span class="f-300">{{ $recipe_fetched->address['full_address'] }}</span>
+                            <hr>
+                            <!-- Recipe Ingredients -->
+                            <h4 class="f-300 m-t-15">Ingredientes:</h4>
+                            <ul class="ingredients m-t-10">
+                                @foreach($recipe_fetched->ingredients as $ingredient)
+                                <li class="item f-300">{{ $ingredient['description'] }}</li>
+                                @endforeach
+                            </ul>
+                            <!-- / Recipe Ingredients -->
 
-                            <!-- Event Share -->
+                            <hr>
+
+                            <!-- Recipe Prep Description -->
+                            <h4 class="f-300 m-t-15">Modo de preparo:</h4>
+                            <div class="m-t-10">
+                                {!!  $recipe_fetched->prep_description !!}
+                            </div>
+                            <!-- / Recipe Prep Description -->
+
+
+                            <!-- Recipe Share -->
                             <h4 class="f-300 m-t-15">Compartilhe:</h4>
                             <div class="m-t-10 m-b-30">
                                 <button type="button" class="btn btn-facebook btn-xs p-5 p-l-10 p-r-10" @click="openShareFacebook()">
@@ -95,39 +227,75 @@
                                     <i class="ion-ios-copy m-r-5"></i>Copiar link
                                 </button>
                             </div>
-                            <!-- / Event Share -->
+                            <!-- / Recipe Share -->
 
                             <hr>
 
-                            <!-- Event Description -->
-                            <h4 class="f-300">Descrição do receita:</h4>
-                            <div class="f-300 m-t-10 m-b-30">
-                                {!! $recipe_fetched->description !!}
-                            </div>
-                            <!-- / Event Description -->
-
-                            <hr>
-
-                            <!-- Event Photos -->
-                            @if(count($recipe_fetched->photos) > 0)
-                                <h4 class="f-300">Imagens do receita:</h4>
-                                <div class="row row-event m-t-10">
-                                    @foreach($recipe_fetched->photos as $photo)
-                                    <div class="col-sm-3 event-col">
-                                        <img class="img-responsive" src="{{ $photo->photo_url }}" alt="">
-                                    </div>
-                                    @endforeach
+                            <!-- Recipe From -->
+                            <h4 class="f-300 m-t-15">Esta receita foi enviada por:</h4>
+                            <div class="text-center" style="width: 120px;">
+                                <div class="picture-circle picture-circle-l m-t-10 m-b-10" style="background-image:url('{{ $recipe_fetched->from->avatar }}')">
                                 </div>
-
-                            @else
-                                <h4 class="f-300">Este receita ainda não possui imagens</h4>
-                            @endif
-                            <!-- Event Photos -->
+                                <span class="f-300">{{ $recipe_fetched->from->full_name }}</span>
+                            </div>
+                            <!-- / Recipe From -->
 
                         </div>
                     </div>
                 </div>
                 <!-- / CENTER COL "ABOUT" -->
+
+                <!-- RIGHT COL "RATINGS" -->
+                <div class="col-sm-3">
+                    <div class="card">
+                        <div class="card-header ch-alt text-center">
+                            <h4 class="m-0 f-300">Últimas avaliações</h4>
+                            <small class="f-300">{{ count($recipe_fetched->ratings) }} {{ count($recipe_fetched->ratings) > 1 ? 'Avaliações' : 'Avaliação' }}</small>
+                        </div>
+                        <div class="card-body card-padding">
+                            @foreach($recipe_fetched->ratings as $ratingIndex => $rating)
+                                @if($ratingIndex < 5)
+                                    <div class="text-center m-t-10">
+                                        <div class="picture-circle picture-circle-p" style="background-image:url('{{ $rating->from->avatar }}')">
+                                        </div>
+                                        <h5 class="f-300 m-t-10 m-b-10">{{ $rating->from->full_name }}</h5>
+                                        @for($i = 1; $i <= 5; $i++)
+                                            @if($i <= $rating->rating)
+                                                <i class="ion-ios-star fa-lg" style="color: #FFCC5F;"></i>
+                                            @else
+                                                <i class="ion-ios-star-outline fa-lg" style="color: #FFCC5F;"></i>
+                                            @endif
+                                        @endfor
+
+                                        @if($ratingIndex + 1 < count($recipe_fetched->ratings))
+                                            <hr class="m-t-30">
+                                        @endif
+                                    </div>
+                                @endif
+                            @endforeach
+
+                            <!-- Call To Download -->
+                            <div class="text-center m-t-30">
+                                <h3 class="f-300">Baixe o <strong style="color: #72c157">isaudavel</strong> e deixe sua avaliação.</h3>
+                                <div class="row">
+                                    <div class="col-sm-12 m-t-20">
+                                        <a href="https://play.google.com/store/apps/details?id=com.isaudavel" target="_blank" title="Faça o download na PlayStore para Android">
+                                            <img class="store-badge img-responsive" src="/images/play_store_btn.png" alt="Faça o download na PlayStore para Android">
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-12 m-t-5">
+                                        <a href="https://itunes.apple.com/us/app/isaudavel/id1277115133?mt=8" target="_blank" title="Faça o download na APP Store para IOS">
+                                            <img class="istore-badge mg-responsive" src="/images/app_store_btn.png" alt="Faça o download na APP Store para IOS">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- / Call To Download -->
+
+                        </div>
+                    </div>
+                </div>
+                <!-- / RIGHT COL "RATINGS" -->
 
             </div>
         </div>
