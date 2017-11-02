571e48aefb202a99a20cfb035264aaf6cb996a06
Igor Trindade <igorlucast@hotmail.com>
2017-11-02 15:24:48 -0200

    Minor fix --merge-and-push

--- app/Listeners/CreateCompanyNotification.php
+++ app/Listeners/CreateCompanyNotification.php
@@ -190,7 +190,7 @@ class CreateCompanyNotification
 
             $notification_data = [
                 'title' => 'Cancelamento de horário',
-                'content' => \Auth::user()->full_name .' remarcou um agendamento do cliente '. $schedule->client->full_name .' de '. $schedule->category->name. ' marcado para ' . $schedule->date . ' ' . $schedule->time . ' na sua agenda.',
+                'content' => \Auth::user()->full_name .' cancelou um agendamento do cliente '. $schedule->client->full_name .' de '. $schedule->category->name. ' marcado para ' . $schedule->date . ' ' . $schedule->time . ' na sua agenda.',
                 'button_label' => 'Visualizar agendamento',
                 'button_action' => '/dashboard/empresas/mostrar/' . $company->id . '/schedule/' . $schedule->id
             ];
